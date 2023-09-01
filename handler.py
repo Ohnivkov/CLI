5c5e07ed2ffa727cd542c5c3f176fda5698ec705
Ohnivkov <136961385+Ohnivkov@users.noreply.github.com>
2023-09-01 10:53:44 +0300

    new_program

--- handler.py
+++ handler.py
@@ -2,38 +2,89 @@ phone_numbers_dict={}
 def input_error(func):
      def inner(*args, **kwargs):
         try:
-            if inner == get_phone:
-                print(func(*args, **kwargs))
-            else:
-                func(*args,**kwargs)
+            return func(*args,**kwargs)
+        except TypeError:
+            return 'wrong command'
+        except IndexError:
+            return 'print: name and number'
         except KeyError:
-            return KeyError
+            return 'Wrong name'
+        except ValueError as e:
+            return e.args[0]
      return inner
-@input_error
-def add_number(name,phone):
-    if is_keyindict(name):
-        raise KeyError
-    else:
-        phone_numbers_dict[name] = phone
-@input_error
-def change_number(name,phone):
 
-    if is_keyindict(name):
-        phone_numbers_dict[name] = phone
+@input_error
+def hello_func():
+    return 'How can i help you'
+
+@input_error
+def add_number(data):
+    name,phone=parse_command(data)
+    if name in phone_numbers_dict:
+        raise ValueError('This contact already exist.')
     else:
-        raise KeyError
+        phone_numbers_dict[name] = phone
+        return 'You added number'
+
+@input_error
+def change_number(data):
+    name,phone=parse_command(data)
+    if name in phone_numbers_dict:
+        phone_numbers_dict[name] = phone
+        return 'You changed number'
+    else:
+        raise ValueError('this contact does not exist')
+
 @input_error
 def get_phone(name):
-    if is_keyindict(name):
+    if name in phone_numbers_dict:
         result=phone_numbers_dict[name]
         return result
     else:
-        raise KeyError
+        raise ValueError('this contact does not exist')
+
+@input_error
 def show_all():
-    for i in phone_numbers_dict:
-        yield f'{i}: {phone_numbers_dict[i]}'
-def is_keyindict(key):
-    if key in phone_numbers_dict:
-        return True
-    else:
-        return False
+    contacts=''
+    for key,value in phone_numbers_dict.items():
+        contacts+=f'{key} : {value} \n'
+    return contacts[0:-2]
+
+@input_error
+def exit_func():
+    return 'good bye'
+
+Command_dict={
+    'hello':hello_func,
+    'exit':exit_func,
+    'close':exit_func,
+    'good bye':exit_func,
+    'add':add_number,
+    'change':change_number,
+    'show all':show_all,
+    'phone':get_phone
+}
+
+def get_command(input):
+    command = input
+    user_input = ''
+    for key in Command_dict:
+        if input.strip().lower().startswith(key):
+            command = key
+            user_input = input[len(command):].strip()
+            break
+    if command:
+        if user_input:
+            return Command_dict[command](user_input)
+        else:
+            return Command_dict[command]()
+    return 'Wrong input'
+def parse_command(command):
+    new_command = command.split(" ")
+    name = new_command[0]
+    phone = new_command[1]
+    if name.isnumeric():
+        raise ValueError('Wrong name.')
+    if not phone.isnumeric():
+        raise ValueError('Wrong phone.')
+    return name, phone
--- main.py
+++ main.py
@@ -1,37 +1,11 @@
 import handler
+def main():
+    while True:
+        user_input = input('Enter command for bot: ')
+        result = handler.get_command(user_input)
+        print(result,end='\n')
+        if result == 'good bye':
+            break
 
-while True:
-    line = input()
-    command = line.split(' ')[0].lower()
-    if line.lower() in ('good bye', 'close', 'exit'):
-        print("Good bye!")
-        break
-    elif line.lower() == 'hello':
-        print("How can I help you?")
-    elif command == 'add':
-        try:
-            name = line.split(' ')[1]
-            phone = line.split(' ')[2]
-            if handler.add_number(name, phone) == KeyError:
-                print('this name already exists in the phone book')
-        except:
-            print("Give me name and phone please")
-    elif command == 'change':
-        try:
-            name = line.split(' ')[1]
-            phone = line.split(' ')[2]
-            if handler.change_number(name, phone) == KeyError:
-                print('there is no such name in the phone book')
-        except:
-            print("Give me name and phone please")
-
-    elif command.lower() == 'phone':
-        try:
-            name = line.split(' ')[1]
-            if handler.get_phone(name)==KeyError:
-                print('there is no such name in the phone book')
-        except:
-            print("Enter name")
-    elif line.lower() == 'show all':
-        for i in handler.show_all():
-            print(i)
+if __name__ == '__main__':
+    main()
