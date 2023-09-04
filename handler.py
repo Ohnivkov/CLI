c11e83903691e0d072be08c91bb541090c00fbe3
Ohnivkov <136961385+Ohnivkov@users.noreply.github.com>
2023-09-04 17:15:02 +0300

    new_program

--- handler.py
+++ handler.py
@@ -1,8 +1,65 @@
-phone_numbers_dict={}
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+
+    def add_phone(self, phone_number):
+        if not self.value:
+            self.value = [phone_number]
+        else:
+            self.value.append(phone_number)
+
+    def remove_phone(self, phone_number):
+        if self.value and phone_number in self.value:
+            self.value.remove(phone_number)
+
+
+class Record:
+
+    def __init__(self, name, phone=None):
+        self.name = Name(value=name)
+        self.phones = Phone(value=[])
+        if phone:
+            self.add_number(phone)
+
+    def add_number(self, phone):
+        self.phones.add_phone(phone)
+
+    def remove_number(self, phone):
+        self.phones.remove_phone(phone)
+
+
+class AdressBook(UserDict):
+    def add_record(self, record: Record):
+        self.data[record.name.value] = record.phones.value
+
+    def remove_record(self, record: Record):
+        del self.data[record.name.value]
+
+    def finder(self, request):
+        found_requests = ''
+        for name, phone in self.data.items():
+            if request in name or request in str(phone):
+                found_requests += f'{name}: {phone}\n'
+        return found_requests
+
+
+adresbook = AdressBook()
+
+
 def input_error(func):
-     def inner(*args, **kwargs):
+    def inner(*args, **kwargs):
         try:
-            return func(*args,**kwargs)
+            return func(*args, **kwargs)
         except TypeError:
             return 'wrong command'
         except IndexError:
@@ -11,80 +68,104 @@ def input_error(func):
             return 'Wrong name'
         except ValueError as e:
             return e.args[0]
-     return inner
+
+    return inner
+
 
 @input_error
 def hello_func():
     return 'How can i help you'
 
-@input_error
-def add_number(data):
-    name,phone=parse_command(data)
-    if name in phone_numbers_dict:
-        raise ValueError('This contact already exist.')
-    else:
-        phone_numbers_dict[name] = phone
-        return 'You added number'
 
 @input_error
 def change_number(data):
-    name,phone=parse_command(data)
-    if name in phone_numbers_dict:
-        phone_numbers_dict[name] = phone
+    name, phone = parse_command(data)
+    if name in adresbook:
+        adresbook[name] = phone
         return 'You changed number'
     else:
         raise ValueError('this contact does not exist')
 
-@input_error
-def get_phone(name):
-    if name in phone_numbers_dict:
-        result=phone_numbers_dict[name]
-        return result
-    else:
-        raise ValueError('this contact does not exist')
 
 @input_error
 def show_all():
-    contacts=''
-    for key,value in phone_numbers_dict.items():
-        contacts+=f'{key} : {value} \n'
+    contacts = ''
+    for key, value in adresbook.items():
+        contacts += f'{key} : {value} \n'
     return contacts[0:-2]
+
 
 @input_error
 def exit_func():
     return 'good bye'
 
-Command_dict={
-    'hello':hello_func,
-    'exit':exit_func,
-    'close':exit_func,
-    'good bye':exit_func,
-    'add':add_number,
-    'change':change_number,
-    'show all':show_all,
-    'phone':get_phone
+
+@input_error
+def class_processor(command, task):
+    if task == 'add':
+        name, phones = parse_command(command)[0], parse_command(command)[1]
+        if name not in adresbook:
+            record = Record(name)
+            for phone in phones:
+                if not phone.isnumeric():
+                    raise ValueError('Wrong phone.')
+                record.add_number(phone)
+            adresbook.add_record(record)
+            return 'Number added'
+        else:
+            raise ValueError('The contact is in adressbook')
+    elif task == 'remove':
+        name = parse_command(command)[0]
+        if name in adresbook:
+            record = Record(name)
+            adresbook.remove_record(record)
+        else:
+            raise ValueError('The contact is not in adressbook')
+    else:
+        request = command
+        if adresbook.finder(request):
+            return adresbook.finder(request)
+        else:
+            return 'no results'
+
+
+Command_dict = {
+    'hello': hello_func,
+    'exit': exit_func,
+    'close': exit_func,
+    'good bye': exit_func,
+    'change': change_number,
+    'show all': show_all,
+    'add': class_processor,
+    'remove': class_processor,
+    'search': class_processor
 }
 
+
 def get_command(input):
-    command = input
+    command = ''
     user_input = ''
     for key in Command_dict:
         if input.strip().lower().startswith(key):
             command = key
             user_input = input[len(command):].strip()
             break
-    if command:
+    if command in ('add', 'search', 'remove'):
+        return Command_dict[command](user_input, command)
+    elif command:
         if user_input:
             return Command_dict[command](user_input)
         else:
             return Command_dict[command]()
-    return 'Wrong input'
+    else:
+        return 'Wrong command'
+
+
 def parse_command(command):
     new_command = command.split(" ")
     name = new_command[0]
-    phone = new_command[1]
+    phone = new_command[1:]
     if name.isnumeric():
         raise ValueError('Wrong name.')
-    if not phone.isnumeric():
-        raise ValueError('Wrong phone.')
+
     return name, phone
